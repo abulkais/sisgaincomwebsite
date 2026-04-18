cd3b3d7fa37eca1638df2aba60445c1783fe24d0
abulkais <abulkaish827@gmail.com>
2026-04-18 18:01:28 +0530

    db file

--- blogs/db_connect.php
+++ blogs/db_connect.php
@@ -7,6 +7,8 @@ $username   = $_ENV['DB_USER'];
 $password   = $_ENV['DB_PASS'];
 $dbname     = $_ENV['DB_NAME'];
 
+
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
